20a51c4e8b64383504b24d21ed14fc24d18cbde8
Anubhav Yadav <yadavanubhav2008@gmail.com>
2026-06-11 18:32:59 +0530

    Day-8 assignment is uploaded

---
+++ Day-8/ProblemNo-31.cpp
@@ -0,0 +1,20 @@
+//Write a program to print character triangle
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+          cout <<char(j + 64)<<" ";
+        }
+        cout << endl;
+    }
+  
+    return 0;
+}
